da30771037037166eb2c5fe5c3a1606b74ef3357
leojavadev <leojavadev@gmail.com>
2020-01-17 21:53:27 -0300

    Implementação do repositório JPA, injeção de dependência e gravação de dados no banco

---
+++ src/main/java/br/com/fling/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.fling.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fling.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
