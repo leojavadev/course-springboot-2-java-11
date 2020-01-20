14b34f487877ed90819eb7f1bce0738ea743c3e4
leojavadev <leojavadev@gmail.com>
2020-01-20 19:31:56 -0300

    Criação da tabela Order

---
+++ src/main/java/br/com/fling/course/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package br.com.fling.course.entities;
+
+public enum OrderStatus {
+
+}
--- src/main/java/br/com/fling/course/entities/User.java
+++ src/main/java/br/com/fling/course/entities/User.java
@@ -1,13 +1,18 @@
 package br.com.fling.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -20,16 +25,20 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")	
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {		
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password, List<Order> orders) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+		this.orders = orders;
 	}
 
 	public Long getId() {
@@ -70,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
