60d709fa8d7f3fa05b4b6e8f6e07d9885b0ac6bc
leojavadev <leojavadev@gmail.com>
2020-01-20 23:03:22 -0300

    Implementação da lista enumerada OrderStatus

--- src/main/java/br/com/fling/course/config/TestConfig.java
+++ src/main/java/br/com/fling/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.fling.course.entities.Order;
 import br.com.fling.course.entities.User;
+import br.com.fling.course.entities.enums.OrderStatus;
 import br.com.fling.course.repositories.OrderRepository;
 import br.com.fling.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456", null);
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456", null);
 		
-		Order o1 = new Order(null, Instant.parse("2020-01-10T10:25:30Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2020-01-11T19:03:45Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2020-01-13T20:02:59Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2020-01-10T10:25:30Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2020-01-11T19:03:45Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2020-01-13T20:02:59Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/br/com/fling/course/entities/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package br.com.fling.course.entities;
-
-public enum OrderStatus {
-
-}
